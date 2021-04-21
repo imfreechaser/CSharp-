a58ba77152c8eb4074eb3b426b308a817e6d81fb
erina <520175084@qq.com>
2021-04-21 23:45:04 +0800

    初始化

---
+++ 必备知识点_控制台相关/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace 必备知识点_控制台相关
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heroX = 0, heroY = 0;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Clear();
+            Console.CursorVisible = false;
+            while (true)
+            {
+                //Console.Clear();
+                Console.Write("■");
+                char inputDirection = Console.ReadKey(true).KeyChar;
+                switch (inputDirection)
+                {
+                    case 'w':
+                    case 'W':
+                        heroY--;
+                        break;
+                    case 's':
+                    case 'S':
+                        heroY++;
+                        break;
+                    case 'A':
+                    case 'a':
+                        heroX -= 2;
+                        break;
+                    case 'D':
+                    case 'd':
+                        heroX += 2;
+                        break;
+                }
+                //Console.WriteLine("\n\n\n\nx,y:{0},{1}",heroX, heroY);
+                Console.SetCursorPosition(heroX, heroY);
+
+                
+                if(inputDirection == 'q')
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
